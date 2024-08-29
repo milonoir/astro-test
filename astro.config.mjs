485eb28ccb41226336422aeb26aeb1d15ea1be10
Milán Boleradszki <bmilan1985@gmail.com>
2024-08-29 22:21:17 +0100

    Make it bilingual and start using components and layouts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,4 +3,11 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://milonoir.github.io',
   base: '/astro-test',
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'hu'],
+    routing: {
+        prefixDefaultLocale: false,
+    },
+  },
 })
