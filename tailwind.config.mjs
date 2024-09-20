f9bd1a0147d15a76ffd4c1f0faa2f886e81f58d3
Milán Boleradszki <bmilan1985@gmail.com>
2024-09-20 23:48:41 +0100

    create logo

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {
 			fontFamily: {
+				abril: ["Abril", "serif"],
 				lora: ["Lora", "serif"],
 				myriad: ["MyriadPro", "sans-serif"],
 				lovelyHome: ["LovelyHome", "sans-serif"],
+			},
+			fontSize: {
+				"2xs": "0.625rem",
+			},
+			colors: {
+				"cream": "#efebe0",
 			},
 		},
 	},
